25f13346fd7ae85f42c99beb86da16be5059b3a7
BearishSun <bearishsun@gmail.com>
2023-03-25 19:03:11 +0100

    Refactor: Missing files from last commit

---
+++ Source/Plugins/bsfVulkanRenderAPI/BsVulkanBuiltinResources.cpp
@@ -0,0 +1,30 @@
+//************************************ bs::framework - Copyright 2023 Marko Pintera **************************************//
+//*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
+#include "BsVulkanBuiltinResources.h"
+#include "BsVulkanGpuDevice.h"
+#include "BsVulkanGpuBuffer.h"
+
+using namespace bs;
+using namespace bs::ct;
+
+VulkanBuiltinResources::VulkanBuiltinResources(VulkanGpuDevice& gpuDevice)
+	:mGpuDevice(gpuDevice)
+{ }
+
+void VulkanBuiltinResources::Initialize()
+{
+	DummyReadBuffer = std::static_pointer_cast<VulkanGpuBuffer>(mGpuDevice.CreateGpuBuffer(GpuBufferCreateInformation::CreateSimpleStorage(BF_32X4F, 1, GpuBufferFlag::StoreOnGPU)));
+	DummyStorageBuffer = std::static_pointer_cast<VulkanGpuBuffer>(mGpuDevice.CreateGpuBuffer(GpuBufferCreateInformation::CreateSimpleStorage(BF_32X4F, 1, GpuBufferFlag::StoreOnGPU | GpuBufferFlag::AllowWritesOnTheGPU)));
+	DummyUniformBuffer = std::static_pointer_cast<VulkanGpuBuffer>(mGpuDevice.CreateGpuBuffer(GpuBufferCreateInformation::CreateUniform(16, GpuBufferFlag::StoreOnGPU)));
+	DummyStructuredBuffer = std::static_pointer_cast<VulkanGpuBuffer>(mGpuDevice.CreateGpuBuffer(GpuBufferCreateInformation::CreateStructuredStorage(16, 1, GpuBufferFlag::StoreOnGPU | GpuBufferFlag::AllowWritesOnTheGPU)));
+	DummyVertexBuffer = std::static_pointer_cast<VulkanGpuBuffer>(mGpuDevice.CreateGpuBuffer(GpuBufferCreateInformation::CreateVertex(16, 1, GpuBufferFlag::StoreOnGPU)));
+}
+
+void VulkanBuiltinResources::Cleanup()
+{
+	DummyReadBuffer = nullptr;
+	DummyStorageBuffer = nullptr;
+	DummyUniformBuffer = nullptr;
+	DummyStructuredBuffer = nullptr;
+	DummyVertexBuffer = nullptr;
+}
---
+++ Source/Plugins/bsfVulkanRenderAPI/BsVulkanBuiltinResources.h
@@ -0,0 +1,48 @@
+//************************************ bs::framework - Copyright 2023 Marko Pintera **************************************//
+//*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
+#pragma once
+
+#include "BsVulkanPrerequisites.h"
+
+namespace bs
+{
+	namespace ct
+	{
+		/** @addtogroup Vulkan
+		 *  @{
+		 */
+
+		/** Contains various resources that are often required by the Vulkan backend. */
+		class VulkanBuiltinResources
+		{
+		public:
+			VulkanBuiltinResources(VulkanGpuDevice& gpuDevice);
+
+			/** Initializes all the builtin resources. */
+			void Initialize();
+
+			/** Destroys all the builtin resources. */
+			void Cleanup();
+
+			/** Buffer that can be used for buffer read operations when no other buffer is bound. */
+			SPtr<VulkanGpuBuffer> DummyReadBuffer;
+
+			/** Buffer that can be used for buffer storage operations when no other buffer is bound. */
+			SPtr<VulkanGpuBuffer> DummyStorageBuffer;
+
+			/** Buffer that can be used for uniform storage when no other buffer is bound. */
+			SPtr<VulkanGpuBuffer> DummyUniformBuffer;
+
+			/** Buffer that can be used for structured storage when no other buffer is bound. */
+			SPtr<VulkanGpuBuffer> DummyStructuredBuffer;
+
+			/** Buffer that can be used for vertex buffers when no other buffer is bound. */
+			SPtr<VulkanGpuBuffer> DummyVertexBuffer;
+
+		private:
+			VulkanGpuDevice& mGpuDevice;
+		};
+
+		/** @} */
+	} // namespace ct
+} // namespace bs
