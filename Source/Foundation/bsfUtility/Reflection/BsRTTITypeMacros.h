9bb487d2611cfb5b0037f7ed35dd4dbba1b31343
Marko Pintera <bearishsun@gmail.com>
2024-08-01 14:23:30 +0200

    Fixed: Documentation

--- Source/Foundation/bsfUtility/Reflection/BsRTTITypeMacros.h
+++ Source/Foundation/bsfUtility/Reflection/BsRTTITypeMacros.h
@@ -58,8 +58,8 @@ namespace bs
 
 /**
  * Registers a new member field in the RTTI type. The field references the @p name member in the owner class.
- * The type of the member must be a valid container type (e.g. vector or map). The container is allowed to contain
- * plain, reflectable and reflectable pointer types alike. Each field must specify a unique ID for @p id.
+ * The type of the member must be a plain, reflectable and reflectable pointer type. Each field must specify
+ * a unique ID for @p id.
  */
 #define B3D_RTTI_MEMBER(name, id) B3D_RTTI_MEMBER_FULL(name, name, id, bs::RTTIFieldInfo::DEFAULT)
 
