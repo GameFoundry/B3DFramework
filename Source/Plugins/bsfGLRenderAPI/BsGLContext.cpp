2021e0400837a5db21dc4ea2c228561023cf7aa4
Marko Pintera <bearishsun@gmail.com>
2025-09-15 11:36:19 +0200

    Refactor: Removed DX11 and OGL render backends

--- Source/Plugins/bsfGLRenderAPI/BsGLContext.cpp
+++
@@ -1,9 +0,0 @@
-//************************************ B3D Framework - Copyright 2018 Marko Pintera **************************************//
-//*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
-#include "BsGLContext.h"
-
-using namespace b3d;
-using namespace b3d::render;
-
-void GLContext::EndCurrent()
-{}
