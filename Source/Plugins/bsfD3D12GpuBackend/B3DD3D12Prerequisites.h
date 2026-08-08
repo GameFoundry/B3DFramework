b1de1b7a25fa239d4e61addb62ecf108e9ee0c08
Marko Pintera <bearishsun@gmail.com>
2026-08-08 11:52:57 +0200

    Feature: Prerequisites

---
+++ Source/Plugins/bsfD3D12GpuBackend/B3DD3D12Prerequisites.h
@@ -0,0 +1,57 @@
+//************************************* B3D Framework - Copyright 2026 Marko Pintera *************************************//
+//*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
+#pragma once
+
+#include "B3DPrerequisites.h"
+
+// Windows headers
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl/client.h>
+
+namespace b3d
+{
+	class D3D12GpuBackend;
+
+	namespace render
+	{
+		class D3D12GpuDevice;
+		class D3D12GpuQueue;
+		class D3D12GpuCommandBuffer;
+		class D3D12GpuCommandBufferPool;
+		struct D3D12GpuCommandBufferSubmitInformation;
+		class D3D12GpuBuffer;
+		class D3D12Texture;
+		class D3D12GpuProgram;
+		class D3D12GpuParameters;
+		class D3D12GpuGraphicsPipelineState;
+		class D3D12GpuComputePipelineState;
+		class D3D12GpuPipelineParameterLayout;
+		class D3D12SamplerState;
+		class D3D12EventQuery;
+		class D3D12GpuQueryPool;
+		class D3D12Framebuffer;
+		class D3D12RenderTexture;
+		class D3D12SwapChain;
+		class D3D12Image;
+		class D3D12ImageSubresource;
+		class D3D12Buffer;
+		class D3D12ResourceManager;
+		class D3D12ResourceTracker;
+		class D3D12BarrierHelper;
+		class D3D12GpuTimelineFence;
+		class D3D12DescriptorHeap;
+		class D3D12DescriptorManager;
+		class D3D12QueryHeap;
+	}
+
+	/** @addtogroup D3D12GpuBackend
+	 *  @{
+	 */
+
+	/** Reference counted pointer to a COM object, releasing it once the last reference goes out of scope. */
+	template<typename T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+	/** @} */
+} // namespace b3d
